b28ae618b4970bb0247d147e9256f8d2e9ec4049
CharlieMarques <Charlie.marques1806@gmail.com>
2024-06-23 19:07:40 -0300

    Agregado del filtro y filtro avanzado , faltan validaciones

--- CatalogoWeb/ListaArticulos.aspx.cs
+++ CatalogoWeb/ListaArticulos.aspx.cs
@@ -5,15 +5,19 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using DataBase;
+using Dominio;
 
 namespace CatalogoWeb
 {
     public partial class ListaArticulos : System.Web.UI.Page
     {
+        public bool filtroAvanzado { get;set;}
         protected void Page_Load(object sender, EventArgs e)
         {
+            filtroAvanzado = false;
             ArticuloDataBase articulo = new ArticuloDataBase();
-            dgvArticulos.DataSource = articulo.toList();
+            Session.Add("listaArticulo", articulo.toList());
+            dgvArticulos.DataSource = Session["listaArticulo"];
             dgvArticulos.DataBind();
         }
 
@@ -26,6 +30,57 @@ namespace CatalogoWeb
         protected void dgvArticulos_PageIndexChanging(object sender, GridViewPageEventArgs e)
         {
             dgvArticulos.PageIndex = e.NewPageIndex;
+            dgvArticulos.DataBind();
+        }
+
+        protected void chkAvanzado_CheckedChanged(object sender, EventArgs e)
+        {
+            filtroAvanzado =chkAvanzado.Checked;
+            txtfiltro.Enabled = !filtroAvanzado;
+        }
+
+        protected void ddlCampo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ddlCriterio.Items.Clear();
+            
+            if(ddlCampo.SelectedItem.ToString()=="Precio")
+            {
+                ddlCriterio.Items.Add("Hasta");
+                ddlCriterio.Items.Add("Mayor a");
+                ddlCriterio.Items.Add("Menor a");
+            }
+            else
+            {
+                ddlCriterio.Items.Add("Contiene");
+                ddlCriterio.Items.Add("Comienza con");
+                ddlCriterio.Items.Add("Termina con");
+            }
+        }
+
+        protected void btnBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ArticuloDataBase articulo = new ArticuloDataBase();
+                dgvArticulos.DataSource = articulo.filtrar(
+                    ddlCampo.SelectedItem.ToString(),
+                    ddlCriterio.SelectedItem.ToString(),
+                    txtFiltroAvanzado.Text);
+                dgvArticulos.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Session.Add("Error", ex);
+                throw;
+            }
+
+        }
+
+        protected void filtro_TextChanged(object sender, EventArgs e)
+        {
+            List<Articulo> lista = (List<Articulo>)Session["listaArticulo"];
+            List<Articulo> listaFiltada = lista.FindAll(x => x.nombre.ToUpper().Contains(txtfiltro.Text.ToUpper()));
+            dgvArticulos.DataSource = listaFiltada;
             dgvArticulos.DataBind();
         }
     }
