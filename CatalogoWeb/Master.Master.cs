3778b326bb2ec5663e7a9d66d2f5ea4e56a09f2d
CharlieMarques <Charlie.marques1806@gmail.com>
2024-06-30 01:27:57 -0300

    Imagenes de perfil

--- CatalogoWeb/Master.Master.cs
+++ CatalogoWeb/Master.Master.cs
@@ -15,12 +15,17 @@ namespace CatalogoWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if(!(Page is Login || Page is Registro || Page is Default))
+            imgAvatar.ImageUrl = "https://simg.nicepng.com/png/small/202-2022264_usuario-annimo-usuario-annimo-user-icon-png-transparent.png";
+            if (!(Page is Login || Page is Registro || Page is Default || Page is Error))
             {
-                if (!SeguridadSession.sessionActiva(Session["Usuario"]))
+                if (!SeguridadSession.sessionActiva(Session["usuario"]))
                 {
                     Response.Redirect("Login.aspx", false);
                 }
+            }
+            if (SeguridadSession.sessionActiva(Session["usuario"]))
+            {
+                imgAvatar.ImageUrl = "~/Images/" + ((Usuarios)Session["usuario"]).ImagenPerfil;
             }
         }
 
--- CatalogoWeb/MiPerfil.aspx.cs
+++ CatalogoWeb/MiPerfil.aspx.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DataBase;
+using Dominio;
+using Seguridad;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +14,57 @@ namespace CatalogoWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                if(!IsPostBack)
+                {
+                    if (SeguridadSession.sessionActiva(Session["usuario"]))
+                    {
+                        Usuarios user = (Usuarios)Session["usuario"];
+                        
+                        txtEmail.Text = user.Email;
+                        txtEmail.ReadOnly = true;
+                        txtNombre.Text = user.Nombre;
+                        txtApellido.Text = user.Apellido;
+                        if (!string.IsNullOrEmpty(user.ImagenPerfil))
+                        {
+                            imgNuevoPerfil.ImageUrl = "~/Images/" + user.ImagenPerfil;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
 
+                throw ex;
+            }
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                UsuariosDataBase userDB = new UsuariosDataBase();
+                string ruta = Server.MapPath("./Images/");
+                Usuarios user = (Usuarios)Session["usuario"];
+                if (txtImagen.PostedFile.FileName != "")
+                {
+                    txtImagen.PostedFile.SaveAs(ruta + "perfil-" + user.id + ".jpg");
+                }
+                user.Nombre = txtNombre.Text;
+                user.Apellido = txtApellido.Text;
+                user.ImagenPerfil = "perfil-" + user.id + ".jpg";
+                userDB.actualizar(user);
+                Image img = (Image)Master.FindControl("imgAvatar");
+                img.ImageUrl = "~/Images" + user.ImagenPerfil;
+            }
+            catch (Exception ex)
+            {
+
+                Session.Add("Error", ex.ToString());
+                Response.Redirect("Error.aspx", false);
+            }
+            
         }
     }
 }
